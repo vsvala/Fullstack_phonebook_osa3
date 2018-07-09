eec070b49406818802fb5a7047584f82f0034c6c
Virva <virva.svala@gmail.com>
2018-07-10 02:17:21 +0300

    cofig dev

--- mongo.js
+++ mongo.js
@@ -1,5 +1,9 @@
 const mongoose = require('mongoose')
 
+if ( process.env.NODE_ENV !== 'production' ) {
+  require('dotenv').config()
+}
+
 const url = process.env.MONGODB_URI
 
 mongoose.connect(url)
