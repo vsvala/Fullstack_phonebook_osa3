ec3e5d5bace1cc22a02d4e23911e86df79ebf432
Virva <virva.svala@gmail.com>
2018-07-10 14:40:00 +0300

    env and lint

---
+++ .vscode/controllers/notes.js
@@ -0,0 +1,6 @@
+const personsRouter = require('express').Router()
+const Person = require('../models/person')
+
+
+
+module.exports = notesRouter
--- index.js
+++ index.js
@@ -22,7 +22,7 @@ app.use(express.static('build'))
 //app.use(logger)
 
 morgan.token('content', function (req, res) {
-return JSON.stringify(req.body)
+  return JSON.stringify(req.body)
 })
 app.use(morgan(':method :url :content :status :res[content-length] - :response-time ms')
 )
@@ -59,34 +59,34 @@ const formatPerson = (person) => {
 
 app.get('/api/persons/:id', (request, response) => {
   Person
-        .findById(request.params.id)
-        .then(person=>{
-          if (person){
-            response.json(formatPerson(person))
-            } else {
-              response.status(404).end()
-            }
-          })
-          .catch(error => {
-            console.log(error)
-            response.status(400).send({ error: 'malformatted id' })
-          })
+    .findById(request.params.id)
+    .then(person => {
+      if (person){
+        response.json(formatPerson(person))
+      } else {
+        response.status(404).end()
+      }
+    })
+    .catch(error => {
+      console.log(error)
+      response.status(400).send({ error: 'malformatted id' })
+    })
           
-        })
+})
 
 app.delete('/api/persons/:id', (request, response) => {
   Person
-  .findByIdAndRemove(request.params.id)
-  .then(result=>{  response.status(204).end()
-  })
-  .catch(error=>{
-    response.status(400).send({error:'malformatted id'})
-})
+    .findByIdAndRemove(request.params.id)
+    .then(result => {  response.status(204).end()
+    })
+    .catch(error => {
+      response.status(400).send( { error:'malformatted id' } )
+    })
 })
 
 
 function getRandomInt(max) {
-  return Math.floor(Math.random() * Math.floor(max));
+  return Math.floor(Math.random() * Math.floor(max))
 }
 
 
@@ -94,10 +94,10 @@ app.post('/api/persons', (request, response) => {
   const body = request.body
 
   if (body.name === undefined) {
-    return response.status(400).json({error: 'name missing'})
+    return response.status(400).json({ error: 'name missing' })
   }
   if (body.num === undefined) {
-    return response.status(400).json({error: 'number missing'})
+    return response.status(400).json({ error: 'number missing' })
   }
 
   const person = new Person({
@@ -107,33 +107,33 @@ app.post('/api/persons', (request, response) => {
     id: getRandomInt(50000)
   })
 
-person
-  .save()
-  .then(formatPerson)
-  .then(savedAndFormattedPerson=> {
-   response.json(savedAndFormattedPerson) 
-})
-.catch(error => {
-  console.log(error)
-  response.status(404).end()
-})
+  person
+    .save()
+    .then(formatPerson)
+    .then(savedAndFormattedPerson => {
+      response.json(savedAndFormattedPerson)
+    })
+    .catch(error => {
+      console.log(error)
+      response.status(404).end()
+    })
 })
 
 app.get('/info', (req, res) => {
   Person
     .find({})
-    .then(persons=>{
-       res.send('<p>puhelinluettelossa on '+ persons.length+' henkilön tiedot</p>'
+    .then(persons => {
+      res.send('<p>puhelinluettelossa on '+ persons.length+' henkilön tiedot</p>'
       +new Date())
     })
 })
 
 app.get('/api/persons', (req, res) => {
-  Person 
-  .find({})
-  .then(persons=>{
-   res.json(persons.map(formatPerson))
-  })
+  Person
+    .find({})
+    .then(persons => {
+      res.json(persons.map(formatPerson))
+    })
 })
 
 
@@ -147,9 +147,9 @@ app.put('/api/persons/:id', (request, response) => {
 
   }
 
-  Person 
+  Person
     .findByIdAndUpdate(request.params.id, person, { new: true })
-    .then(updatedPerson=> {
+    .then(updatedPerson => {
       response.json(formatPerson(updatedPerson))
     })
     .catch(error => {
